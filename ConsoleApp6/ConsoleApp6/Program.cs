debf2d75321517a119b063548f0a2b406df6b70f
종철 이 <mylife68@nate.com>
2019-01-12 12:29:16 +0900

    연산자 오버로딩, ToString, Equals

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ConsoleApp6{
+    class Adder : Object   {
+        public int Value { get; set; }
+        public static Adder operator + (Adder a1, Adder a2) {
+            Adder a3 = new Adder();
+            a3.Value = a1.Value + a2.Value;
+            return a3;
+        }      
+        public override string ToString() {   return $"Value : {Value}";    }
+        public override bool Equals(object a2)         {
+            return this.Value == ((Adder)a2).Value;
+        }
+    }
+    class Program     {
+        static void Main(string[] args)         {
+            Adder a1 = new Adder(); a1.Value = 1;
+            Adder a2 = new Adder(); a2.Value = 1;
+            Adder a3 = a1 + a2;      Console.WriteLine(a3);
+            if (a1 == a2) Console.WriteLine("a1 == a2");  
+            if (a1.Equals(a2))  Console.WriteLine("a1.equals(a2)");
+
+        }
+    }
+}
---
+++ GenericExam/GenericExam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace GenericExam
+{
+    //일반화 클래스
+    class SumTest<T>    {
+        internal int Sum(T i, T j)   {           return (dynamic)i +(dynamic)j;     }
+    }
+    //일반화 메소드
+    class SumTest2    {
+        internal int Sum<T>(T i, T j) { return (dynamic)i + (dynamic)j; }
+    }
+    class Program    {
+        static void Main(string[] args)        {
+            SumTest<int> s1 = new SumTest<int>();
+            Console.WriteLine(s1.Sum(1, 2));
+            SumTest2 s2 = new SumTest2();
+            Console.WriteLine(s2.Sum<int>(1, 2));
+        }
+    }
+}
