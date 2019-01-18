ee03ea109f5435631d5bafd1b6c23a74c4f88c64
종철 이 <mylife68@nate.com>
2019-01-18 16:25:27 +0900

    C# 이벤트 테스트

--- DelagateTest3/DelagateTest3/Program.cs
+++ DelagateTest3/DelagateTest3/Program.cs
@@ -15,12 +15,18 @@ class Program
         // Call via a delegate
         // MyClass의 GetCallback(Callback callBack) 메소드를 호출하여
         // MyMethod1, MyMethod2가 호출 되도록 코드를 완성하세요
-        Callback c1 = my.MyMethod1;
-        c1 += my.MyMethod2;
+        my.GetCallback( my.MyMethod1 );
+        my.GetCallback( my.MyMethod2 );
+
+        Callback c = my.MyMethod1;
+        c += my.MyMethod2;
+        c();
+
+        Callback c1 = (Callback)Delegate.Combine( 
+                                                     new Callback(my.MyMethod1),
+                                                     new Callback(my.MyMethod2));
         c1();
 
-        my.GetCallback(my.MyMethod1);
-        my.GetCallback(my.MyMethod1);
     }
 }
 public class MyClass
---
+++ EventTest1/EventTest1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    //이벤트 게시자 클래스, 이벤트를 발생시키는 객체
+    class EventPublisher
+    {
+        public delegate void MyEventHandler(); //이벤트처리를위한델리게이트정의
+        MyEventHandler handler;
+        public event MyEventHandler MyEvent
+        {
+            add
+            {
+                handler += value;
+            }
+            remove
+            {
+                handler -= value;
+            }
+        }
+
+        public void Do()
+        {
+            //MyEvent?.Invoke();
+            //MyEvent();
+            handler();
+            handler?.Invoke();
+        }
+    }
+
+    //구독자 클래스
+    class Subscriber
+    {
+        static void Main(string[] args)
+        {           
+            EventPublisher p = new EventPublisher();
+            p.MyEvent += new EventPublisher.MyEventHandler(doAction);
+            p.Do();
+        }
+        //MyEvent 라는 이벤트가 발생하면 호출되는 메소드
+        static void doAction()
+        {
+            Console.WriteLine("MyEvent 라는 이벤트 발생...");
+        }
+    }
+}
