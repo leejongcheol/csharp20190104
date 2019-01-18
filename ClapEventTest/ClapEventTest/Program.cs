e3f41e2840fd6a5f2dfc1684000d4e67e0f4f359
종철 이 <mylife68@nate.com>
2019-01-18 16:49:11 +0900

    C# 이벤트, 짝수일때만 이벤트를 호출히여 "짝" 출력

---
+++ ClapEventTest/ClapEventTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClapEventTest
+{
+    class Program
+    {
+        public delegate void ClapDelegate(int i);
+        public event ClapDelegate ClapEvent;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.ClapEvent += new ClapDelegate(p.Clap);
+            Console.Write("숫자를 입력하세요. ");
+            int num = int.Parse(Console.ReadLine());
+
+            for(int i=1; i <= num; i++)   {  if (i % 2 == 0) p.ClapEvent(i);   }
+        }
+
+        void Clap(int n)
+        {
+            Console.WriteLine($"짝({n})");
+        }
+    }
+}
