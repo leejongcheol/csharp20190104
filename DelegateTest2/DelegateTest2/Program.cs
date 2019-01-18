64f7660a6fc0bf490e8aa573aedf6127735b9406
종철 이 <mylife68@nate.com>
2019-01-18 15:07:28 +0900

    델리게이트 실습

---
+++ DelegateTest2/DelegateTest2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    delegate void Deli(string s);
+    static void Main()
+    {
+        Deli d = delegate (string s)
+                    {
+                        Console.WriteLine(s);
+                    };
+        d( "안녕" );
+        d.Invoke("안녕");
+
+        // Specify delegate with Lambda Expression
+        Deli d0 = (s) => Console.WriteLine(s);
+
+        //Invoke delegate.
+        d0.Invoke("OJC");
+        // Specify delegate with new Constructor
+        Deli d1 = new Deli(d);
+        d1.Invoke("OJC");
+        // Specify delegate with method name
+        Deli d2 = d;
+        d2.Invoke("OJC");
+    }
+    static void d(string s)
+    {
+        Console.WriteLine(s);
+    }
+}
