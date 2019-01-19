c144777332831da8ed82bfbc7baef31df58bc906
종철 이 <mylife68@nate.com>
2019-01-19 09:42:31 +0900

    TcpClient, TcpListener 실습, 클라이언트는 서버로 글을 보내기만 하고, 서버는 글을 받아서 출력만 하는 간단한 예제 를 작성해 보자.

---
+++ Client/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+class TcpClientTest
+{
+    static void Main(string[] args)
+    {
+        TcpClient client = null;
+        try
+        {
+            //LocalHost에 지정 포트로 TCP Connection을 생성하고 데이터를 송수신  하기
+             //위한 스트림을 얻는다.
+             client = new TcpClient();
+            client.Connect("localhost", 5001);
+            NetworkStream stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+            string dataToSend = Console.ReadLine();
+            while (true)
+            {
+                writer.WriteLine(dataToSend);
+                if (dataToSend.IndexOf("<EOF>") > -1) break;
+                dataToSend = Console.ReadLine();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
